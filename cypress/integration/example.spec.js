48e413199f6a8fd15b4c66541ac445ca3c4a4400
PMafra <pmafrarm@gmail.com>
2021-12-14 17:07:43 -0300

    Add cypress test example

---
+++ cypress/integration/example.spec.js
@@ -0,0 +1,14 @@
+// describe('Sign-up', () => {
+//   it('Should sign-up sucessfully', () => {
+//     cy.visit('http://localhost:3000/sign-up');
+
+//     cy.get('input[placeholder=Password]').type('Gratibox123@');
+//     cy.get('input[placeholder="Password confirmation"]').type('Gratibox123@');
+
+//     cy.contains('Sign-up').click();
+
+//     cy.get('#display-message').should(($message) => {
+//       expect($message).to.contain('Account created successfully!');
+//     });
+//   });
+// });
