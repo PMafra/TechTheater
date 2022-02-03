e3c74f5a0bcc0aa747e1d64d59943d4e5c640b41
PMafra <pmafrarm@gmail.com>
2022-02-03 18:21:06 -0300

    feat: finish basic topbar for homepage

--- src/pages/HomePage/index.js
+++ src/pages/HomePage/index.js
@@ -6,23 +6,27 @@ import {
 } from 'react-router-dom';
 import Recommendations from './Recommendations/index';
 import HomePageLayout from '../../components/layouts/HomePage';
+import Navbar from '../../components/HomePage/Navbar';
 
 export default function HomePage() {
   const { pathname } = useLocation();
 
   return (
-    <HomePageLayout>
-      <Routes>
-        <Route
-          path="/"
-          element={<Navigate to={`${pathname}/recommendations`} />}
-        />
-        <Route
-          exact
-          path="/recommendations"
-          element={<Recommendations />}
-        />
-      </Routes>
-    </HomePageLayout>
+    <>
+      <Navbar />
+      <HomePageLayout>
+        <Routes>
+          <Route
+            path="/"
+            element={<Navigate to={`${pathname}/recommendations`} />}
+          />
+          <Route
+            exact
+            path="/recommendations"
+            element={<Recommendations />}
+          />
+        </Routes>
+      </HomePageLayout>
+    </>
   );
 }
