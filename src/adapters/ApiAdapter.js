49b754b991153f964349262aea7ae809d3fcebee
PMafra <pmafrarm@gmail.com>
2022-03-06 17:16:07 -0300

    feat: start creating adapter for movies api

---
+++ src/adapters/ApiAdapter.js
@@ -0,0 +1,17 @@
+// import api from '../services/api';
+
+// export default class ApiAdapter {
+//   constructor() {
+//     this.
+//     this.topMovies
+//   }
+
+//   getTopMovies() {
+
+//     return api.get(`/MostPopularMovies/${process.env.REACT_APP_IMDB_KEY || 'k_0bxzff2h'}`);
+//   }
+
+//   getTopTVShows() {
+//     return api.get(`/MostPopularTvs/${process.env.REACT_APP_IMDB_KEY || 'k_0bxzff2h'}`);
+//   }
+// }
